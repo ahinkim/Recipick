4df7c105c3881a76dd33c707f6bdf365777cb593
ahinkim <ahin98@naver.com>
2021-10-28 18:03:29 +0900

    Add jwt to login in views.py

--- Backend/serverProject/views.py
+++ Backend/serverProject/views.py
@@ -1,17 +1,26 @@
+from os import access
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.parsers import JSONParser
-# import jwt
 
-# from django.shortcuts import render
-# from django.core import serializers
-# from django.http import HttpResponse
-# from rest_framework.decorators import api_view, permission_classes, authentication_classes
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-# import jwt
+import jwt
+import datetime
+
+import json
+from django.core.exceptions import ImproperlyConfigured
+with open("../secret.json") as f:
+    secrets = json.loads(f.read())
+
+#외부에 저장한 SECRETKEY 불러오는 함수
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = f"Set the {setting} enviroment variable"
+        raise ImproperlyConfigured(error_msg)
+
 #계정 중복 확인(GET), 회원가입(POST)
 @csrf_exempt
 def user_list(request):
@@ -21,16 +30,16 @@ def user_list(request):
 
         #중복인 경우
         if User.objects.filter(userId=search_userId).exists():
-            return HttpResponse(status=401)
+            return JsonResponse({'message': 'FALSE'}, status=401)
         else: #중복이 아닌 경우
-            return HttpResponse(status=200)
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return HttpResponse(status=200)
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
         return JsonResponse(serializer.errors, status=401)
 
 
@@ -44,7 +53,7 @@ def user_list(request):
 #     if request.method == 'DELETE':
 #         obj.delete()
 #         return HttpResponse(status=204)
-
+#     if access.decode('utf-8') == access.decode('utf-8'): -->user 비교할 때 꼭 이렇게 비교하기
 
 #login - 로그인(POST)
 @csrf_exempt
@@ -56,11 +65,15 @@ def login(request):
             obj = User.objects.get(userId=search_userId)
             
             if data['password'] == obj.password: #비밀번호 일치
-                return HttpResponse(status=200)
+                secret = get_secret("SECRET_KEY")
+                #weeks=2로 바꾸기
+                access = jwt.encode({"exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=300),"userId": data['userId']}, secret, algorithm="HS256")
+
+                return JsonResponse({"accessToken": access.decode('utf-8')},status=200)
             else:  #비밀번호 불일치
-                return HttpResponse(status=401)
+                return JsonResponse({'message': 'FALSE'}, status=401)
         else: #없는 아이디인 경우
-            return HttpResponse(status=401)
+            return JsonResponse({'message': 'FALSE'}, status=401)
         
 
 #accessToken 발급
